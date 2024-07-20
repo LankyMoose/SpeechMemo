52fde15389001bd86f5c3ddc2a459e4d673da502
Robby6Strings <rausten93@gmail.com>
2024-07-20 21:41:16 +1200

    improve VoiceSelector

--- src/components/VoicesSelector.tsx
+++ src/components/VoicesSelector.tsx
@@ -1,5 +1,5 @@
 import { useVoices } from "$/context/VoicesContext"
-import { Transition } from "kaioken"
+import { Transition, useEffect, useRef } from "kaioken"
 
 export const VoiceSelector = () => {
   const {
@@ -14,29 +14,38 @@ export const VoiceSelector = () => {
     <Transition
       in={selectorOpen}
       element={(state) => {
+        const ref = useRef<HTMLDivElement | null>(null)
         if (state === "exited") return null
-        if (state === "entered") {
-          document.body.style.overflow = "hidden"
-        } else {
-          document.body.style.overflow = "auto"
-        }
+        useEffect(() => {
+          if (state === "entered") {
+            document.body.style.overflow = "hidden"
+            const handler = (e: Event) => {
+              if (ref.current && !ref.current.contains(e.target as Node)) {
+                setSelectorOpen(false)
+              }
+            }
+            document.addEventListener("click", handler)
+            return () => document.removeEventListener("click", handler)
+          } else {
+            document.body.style.overflow = "auto"
+          }
+        }, [state])
         return (
           <div
+            ref={ref}
             style={{
               opacity: state === "entered" ? "1" : "0",
-              translate: `0 ${state === "entered" ? "0" : "-100%"}`,
+              translate: `0 ${state === "entered" ? "0" : "100%"}`,
               overflowY: state === "entered" ? "auto" : "hidden",
             }}
-            className="z-[9999999999] fixed top-0 w-screen max-h-screen max-w-full transition-all bg-[#000b] backdrop-blur flex flex-col p-[2px] gap-[2px] rounded-b-md"
+            className="z-[9999999999] fixed right-0 sm:rounded-lg sm:right-16 bottom-0 sm:bottom-16 w-screen max-w-screen sm:w-[400px] h-screen max-h-screen sm:max-h-[600px] transition-all bg-[#000b] backdrop-blur flex flex-col p-[2px] gap-[2px]"
           >
             {voices.map((voice) => (
               <button
                 key={voice.name}
                 className={`${
-                  voice === selectedVoice
-                    ? "text-emerald-200"
-                    : "text-emerald-50"
-                } text-sm hover:text-emerald-200 text-nowrap bg-[#ffffff05] px-4 py-2 rounded-md`}
+                  voice === selectedVoice ? "text-blue-300" : "text-indigo-50"
+                } text-sm hover:text-blue-300 text-nowrap bg-[#ffffff05] px-4 py-2 rounded-md`}
                 onclick={() => {
                   setSelectedVoice(voice)
                   setSelectorOpen(false)
